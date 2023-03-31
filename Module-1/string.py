abc6949f46c188201264fbe2364c06f1e043396d
SanketTops <sanket.tops@gmail.com>
2023-03-31 16:15:53 +0530

    Module-1

---
+++ Module-1/string.py
@@ -0,0 +1,32 @@
+mystr='this is python!'
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #slice *(range)
+print(mystr[5:])
+print(mystr[:4])
+
+print(len(mystr))"""
+
+# ------------------------------------------------- #
+
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('P','Y'))
+#print(mystr.split())
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('is'))
+#print(mystr.startswith('t'))
+#print(mystr.endswith('!'))
+#print(mystr.find('Python'))
+#print(mystr.index('P'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+#print(mystr.title())
