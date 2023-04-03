c83035880ba08bd4e015d19ecd01aa316e34997e
SanketTops <sanket.tops@gmail.com>
2023-04-03 16:24:30 +0530

    Collection Type

---
+++ Module-2_Collection/list_example.py
@@ -0,0 +1,57 @@
+tech=['Python','PHP','JAVA','Ruby','Perl','Angular']
+
+"""print(tech)
+print(tech[0])
+print(tech[-1])
+print(tech[0:4])
+print(tech[2:])
+print(tech[:3])
+
+print(len(tech))"""
+
+"""if 'PHP' in tech:
+    print("Yes...")
+else:
+    print("Noo")"""
+
+#print(tech)
+#tech[1]='Android'
+#print(tech)
+
+#python=0
+#php=1
+#java=2
+
+"""n=0
+for i in tech:
+    #print(i)
+    print(f"{i}={n}")
+    n+=1"""
+    
+"""print(tech.index('Perl'))
+
+for i in tech:
+    #print(i)
+    print(f"{i}={tech.index(i)}")"""
+    
+# ------------------------------------------------------ #
+print(tech)
+tech.append('iOS')
+tech.insert(0,'Android')
+#tech.pop()
+#tech.pop(2)
+#tech.clear()
+#tech.remove('JAVA')
+#del tech[1]
+#del tech
+#tech.reverse()
+#tech.sort()
+#print(tech)
+
+#newtech=tech.copy()
+#print(newtech)
+
+newtech=['C','C++','DS','C#']
+#print(tech+newtech)
+#tech.extend(newtech)
+#print(tech)
---
+++ Module-2_Collection/list_userinput.py
@@ -0,0 +1,14 @@
+#tech=[]
+tech=list()
+
+n=int(input("Enter number of elements:"))
+
+for i in range(n):
+    x=input("Enter your element:")
+    tech.append(x)
+
+print(tech)
+
+
+
+
