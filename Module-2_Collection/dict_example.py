7da009d9f1c7e99d9437fb217f64fc17cbe42d04
SanketTops <sanket.tops@gmail.com>
2023-04-10 16:31:39 +0530

    Collection types

--- Module-2_Collection/dict_example.py
+++ Module-2_Collection/dict_example.py
@@ -1,7 +1,44 @@
 stdata={'id':101,'name':'sanket','city':'rajkot'}
-print(stdata)
-print(stdata['city'])
-print(stdata.get('name'))
+"""print(stdata)
+print(stdata['name'])
+print(stdata.get('city'))
+
 print(len(stdata))
 print(stdata.keys())
-print(stdata.values())
+print(stdata.values())"""
+
+# ---------------------------------------------- #
+#print(stdata)
+
+"""if 'sanket' in stdata.values():
+    print("Yes...")
+else:
+    print("Noo")"""
+
+#stdata['id']=102
+#stdata['sub']='Python'
+print(stdata)
+
+"""for i in stdata:
+    print(i)"""
+
+"""for i in stdata.values():
+    print(i)"""
+
+"""for i in stdata.items():
+    print(i)"""
+
+#Key=id and Values=101
+#Key=name and Values=sanket
+
+"""for i,j in stdata.items():
+    print(f"Key={i} and Values={j}")"""
+
+
+#stdata.pop('name')
+#del stdata['city']
+#stdata.clear()
+print(stdata)
+
+#newdict=stdata.copy()
+#print(newdict)
---
+++ Module-2_Collection/dict_userinput.py
@@ -0,0 +1,36 @@
+#stdata={}
+"""stdata=dict()
+
+n=int(input("Enter number of pairs:"))
+
+for i in range(n):
+    key=input("Enter your key's:")
+    value=input("Enter your value's:")
+    stdata[key]=value
+
+print(stdata)"""
+
+# -------------------------------------------- #
+stdata=dict()
+
+key=['id','name','sub']
+
+
+"""for i in range(len(key)):
+    value=input("Enter your value:")
+    stdata[i]=value"""
+
+"""for i in key:
+    value=input(f"Enter your value for {i}:")
+    stdata[i]=value
+print(stdata)"""
+
+"""for i in key:
+    print(i)"""
+
+
+#stdata{{'id':1,'name':'sanket'},{'id':1,'name':'sanket'},{'id':1,'name':'sanket'}}
+
+#stdata={'id':[1,2,3],'name':['sanket','nirav','ashok']}
+
+stdata={'1':{'id':1,'name':'sanket'},'2':{'id':1,'name':'sanket'}}
