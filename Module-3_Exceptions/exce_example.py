9c561869052d6b50b6ef6f11bb902f2419dc3868
SanketTops <sanket.tops@gmail.com>
2023-04-24 16:34:50 +0530

    Files + exceptions

---
+++ Module-3_Exceptions/exce_example.py
@@ -0,0 +1,12 @@
+a=34
+b=67
+
+try:
+    print("Sum:",a+B)
+except:
+    print("Error!")
+
+
+x=56
+y=23
+print("Mul:",x*y)
---
+++ Module-3_Files/folder_create.py
@@ -0,0 +1,32 @@
+import os
+
+#os.mkdir('Myfolder')
+
+"""os.chdir('Newfolder/Subfolder')
+os.mkdir('Myfolder')"""
+
+"""os.chdir('Newfolder/Subfolder/Myfolder')
+open('myfile.txt','x')"""
+
+
+"""os.chdir('Newfolder/Subfolder/Myfolder')
+if os.path.exists('myfile.txt'): #true
+    print("File is already exists!")
+else:
+    open('myfile.txt','x')
+    print("New file created!")"""
+
+#os.removedirs('Newfolder')
+
+#os.chdir('Newfolder/Subfolder/Myfolder')
+#os.remove('myfile.txt')
+
+
+#os.chdir('Newfolder')
+#os.removedirs('Newfolder')
+
+
+#os.system('notepad')
+
+#os.startfile('chrome')
+os.startfile('WINWORD')
