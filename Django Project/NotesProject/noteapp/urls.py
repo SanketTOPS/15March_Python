236aea42e24366ce43a211a073d34e827c2a6c31
SanketTops <sanket.tops@gmail.com>
2023-07-06 12:03:14 +0530

    Index Page

--- Django Project/NotesProject/noteapp/urls.py
+++ Django Project/NotesProject/noteapp/urls.py
@@ -4,4 +4,8 @@ from noteapp import views
 
 urlpatterns = [
     path('',views.index),
+    path('about/',views.about),
+    path('contact/',views.contact),
+    path('profile/',views.profile),
+    path('notes/',views.notes),
 ]
--- Django Project/NotesProject/noteapp/views.py
+++ Django Project/NotesProject/noteapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .forms import signupForm
 
 # Create your views here.
 
 def index(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+        else:
+            print(newuser.errors)
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def notes(request):
+    return render(request,'notes.html')
+
+def profile(request):
+    return render(request,'profile.html')
